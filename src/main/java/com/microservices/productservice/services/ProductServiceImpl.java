d8ee25c2a2481430d0e6f2954bfa6b8d9713b862
Konstantinos Paidarakis <kpaidarakis.external@epo.org>
2025-07-14 14:20:22 +0300

    add logging to service

--- src/main/java/com/microservices/productservice/services/ProductServiceImpl.java
+++ src/main/java/com/microservices/productservice/services/ProductServiceImpl.java
@@ -4,11 +4,13 @@ import com.microservices.productservice.entities.Converter;
 import com.microservices.productservice.entities.ProductEntity;
 import com.microservices.productservice.model.Product;
 import com.microservices.productservice.repositories.ProductRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Log4j2
 public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
@@ -19,6 +21,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public List<Product> getAllProducts() {
+        log.info("=> Getting all products");
         List<ProductEntity> productEntities = productRepository.findAll();
         return productEntities.stream()
                 .map(Converter::convertFromEntity)
@@ -28,7 +31,9 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public Long addProduct(Product product) {
         ProductEntity productEntity = Converter.convertToEntity(product);
+        log.info("=> Adding product: {}", productEntity);
         productRepository.save(productEntity);
+        log.info("=> Product Added: {}", productEntity);
         return productEntity.getProductId();
     }
 }
